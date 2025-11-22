706a7119158b888395a9a125970d893acffd545d
isamu <isamu@to-kyo.to>
2025-11-22 15:56:39 +0900

    separate node and node base

--- packages/grapys-vue/src/views/Node.vue
+++ packages/grapys-vue/src/views/Node.vue
@@ -1,64 +1,39 @@
 <template>
-  <div
-    class="absolute flex w-36 cursor-grab flex-col rounded-md text-center text-white select-none"
-    :class="nodeMainClass(expectNearNode, nodeData)"
-    :style="transform"
-    ref="thisRef"
-    @mousedown="onStartNode"
-    @touchstart="onStartNode"
-    @dblclick="(e) => openNodeMenu(e)"
-    @click="(e) => openNodeEditMenu(e)"
+  <NodeBase
+    :node-data="nodeData"
+    :nearest-data="nearestData"
+    :inputs="inputs"
+    :outputs="outputs"
+    :nested-graph-id="nestedGraph?.id"
+    :is-connectable="isConnectable"
+    @update-position="(data) => $emit('updatePosition', data)"
+    @save-position="$emit('savePosition')"
+    @new-edge-start="(data) => $emit('newEdgeStart', data)"
+    @new-edge="(data) => $emit('newEdge', data)"
+    @new-edge-end="$emit('newEdgeEnd')"
+    @open-node-menu="(e) => $emit('openNodeMenu', e)"
+    @open-node-edit-menu="(e) => { if (outerMenu) { $emit('openNodeEditMenu', e) }}"
+    @node-drag-start="$emit('nodeDragStart')"
+    @node-drag-end="$emit('nodeDragEnd')"
   >
-    <!-- HEAD -->
-    <div>
+    <template #header="{ nodeData, expectNearNode }">
       <div class="w-full rounded-t-md py-1 text-center leading-none" :class="nodeHeaderClass(expectNearNode, nodeData)">
         {{ nodeData.nodeId }}
       </div>
       <div class="w-full py-1 text-center text-xs leading-none" v-if="nodeData.type === 'computed'" :class="nodeHeaderClass(expectNearNode, nodeData)">
         {{ nodeData.data.guiAgentId?.replace(/Agent$/, "") }}
       </div>
-    </div>
-    <!-- Body -->
-    <div>
-      <!-- body head -->
+    </template>
+
+    <template #body-head>
       <div v-if="agentProfile.agents && innerMenu">
         <select v-model="agentIndex" @change="updateAgentIndex">
           <option :value="key" v-for="(agent, key) in agentProfile.agents" :key="key">{{ agent }}</option>
         </select>
       </div>
-      <!-- right output -->
-      <div class="mt-1 flex flex-col items-end">
-        <div v-for="(output, index) in outputs" :key="['out', output.name, index].join('-')" class="relative flex items-center" ref="outputsRef">
-          <span class="mr-2 text-xs whitespace-nowrap">{{ output.name }}</span>
-          <div
-            class="absolute right-[-10px] h-4 w-4 min-w-[12px] rounded-full"
-            :class="nodeOutputClass(isExpectNearButton('inbound', index), nodeData, isConnectable)"
-            @click.stop
-            @mousedown.stop.prevent="(e) => onStartEdge(e, 'outbound', index)"
-            @touchstart.stop.prevent="(e) => onStartEdge(e, 'outbound', index)"
-          ></div>
-        </div>
-      </div>
+    </template>
 
-      <!-- left input -->
-      <div class="mt-1 mb-1 flex flex-col items-start">
-        <div
-          v-for="(input, index) in inputs"
-          :key="['in', input.name, index, nestedGraph?.id ?? ''].join('-')"
-          class="relative flex items-center"
-          ref="inputsRef"
-        >
-          <div
-            class="absolute left-[-10px] h-4 w-4 min-w-[12px] rounded-full"
-            :class="nodeInputClass(isExpectNearButton('outbound', index), nodeData, input as any, isConnectable)"
-            @click.stop
-            @mousedown.stop.prevent="(e) => onStartEdge(e, 'inbound', index)"
-            @touchstart.stop.prevent="(e) => onStartEdge(e, 'inbound', index)"
-          ></div>
-          <span class="ml-2 text-xs whitespace-nowrap">{{ input.name }}</span>
-        </div>
-      </div>
-      <!-- Body -->
+    <template #body-main="{ focusEvent, blurEvent }">
       <div class="flex w-full flex-col gap-1 p-2" v-if="nodeData.type === 'static' && innerMenu">
         <NodeStaticValue :node-data="nodeData" @focus-event="focusEvent" @blur-event="blurEvent" @update-static-value="updateStaticValue" />
       </div>
@@ -73,19 +48,19 @@
           <option :value="key" v-for="(graph, key) in nestedGraphs" :key="key">{{ graph.name }}</option>
         </select>
       </div>
-    </div>
-  </div>
+    </template>
+  </NodeBase>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, watchEffect, computed, PropType, onMounted, watch } from "vue";
+import { defineComponent, ref, computed, PropType, watch } from "vue";
 import { useStore } from "../store";
-import type { GUINodeData, GUINearestData, NewEdgeEventDirection } from "../utils/gui/type";
-import { getClientPos, getNodeSize, getTransformStyle, nestedGraphInputs } from "../utils/gui/utils";
+import type { GUINodeData, GUINearestData } from "../utils/gui/type";
+import { nestedGraphInputs } from "../utils/gui/utils";
 import { agentProfiles, staticNodeParams } from "../utils/gui/data";
-import { nodeMainClass, nodeHeaderClass, nodeOutputClass, nodeInputClass } from "../utils/gui/classUtils";
-// import { graphs } from "../graph";
+import { nodeHeaderClass } from "../utils/gui/classUtils";
 
+import NodeBase from "./NodeBase.vue";
 import NodeStaticValue from "./NodeStaticValue.vue";
 import NodeComputedParams from "./NodeComputedParams.vue";
 import NodeResult from "./NodeResult.vue";
@@ -93,6 +68,7 @@ import NodeResult from "./NodeResult.vue";
 export default defineComponent({
   name: "Node",
   components: {
+    NodeBase,
     NodeStaticValue,
     NodeComputedParams,
     NodeResult,
@@ -129,184 +105,24 @@ export default defineComponent({
     "nodeDragStart",
     "nodeDragEnd",
   ],
-  setup(props, ctx) {
+  setup(props, { emit }) {
     const store = useStore();
 
+    // Agent profile
     const agentProfile = props.nodeData.type === "computed" ? agentProfiles[props.nodeData.data.guiAgentId ?? ""] : staticNodeParams;
 
-    const thisRef = ref<HTMLElement | null>(null);
-    const inputsRef = ref<HTMLElement[]>([]);
-    const outputsRef = ref<HTMLElement[]>([]);
+    // State
     const innerMenu = ref(false);
     const outerMenu = ref(true);
-
-    const isDragging = ref(false);
-    const isNewEdge = ref(false);
-    const offset = ref({ x: 0, y: 0 });
-
     const agentIndex = ref(props.nodeData.data.agentIndex ?? 0);
     const nestedGraphIndex = ref(props.nodeData.data.nestedGraphIndex ?? 0);
 
-    const startPosition = { x: 0, y: 0 };
-    // If it moves only a little, the data will not be saved because it stack much more histories.
-    let deltaDistance = 0; // square
-    const deltaDistanceThredhold = 4;
-
-    const onStartNode = (event: MouseEvent | TouchEvent) => {
-      if (isNewEdge.value) {
-        return;
-      }
-      isDragging.value = true;
-      ctx.emit("nodeDragStart");
-      const { clientX, clientY } = getClientPos(event);
-      const position = props.nodeData.position;
-      offset.value.x = clientX - position.x;
-      offset.value.y = clientY - position.y;
-
-      // for update detection
-      startPosition.x = position.x;
-      startPosition.y = position.y;
-      deltaDistance = 0;
-    };
-
-    // some time inputsRef/outputsRef order is broken when nestedGraph change.
-    const sortedInputs = computed(() => {
-      return [...inputsRef.value].sort((aa, bb) => (aa.getBoundingClientRect().top > bb.getBoundingClientRect().top ? 1 : -1));
-    });
-    const sortedOutputs = computed(() => {
-      return [...outputsRef.value].sort((aa, bb) => (aa.getBoundingClientRect().top > bb.getBoundingClientRect().top ? 1 : -1));
-    });
-    const getWH = () => {
-      return getNodeSize(thisRef.value, sortedInputs.value, sortedOutputs.value);
-    };
-    onMounted(() => {
-      ctx.emit("updatePosition", getWH());
-    });
-
-    const onMoveNode = (event: MouseEvent | TouchEvent) => {
-      if (!isDragging.value) return;
-      const { clientX, clientY } = getClientPos(event);
-      const x = clientX - offset.value.x;
-      const y = clientY - offset.value.y;
-      const newPosition = { ...getWH(), x, y };
-      ctx.emit("updatePosition", newPosition);
-      deltaDistance = (startPosition.x - x) ** 2 + (startPosition.y - y) ** 2;
-    };
-
-    const onEndNode = () => {
-      isDragging.value = false;
-      ctx.emit("nodeDragEnd");
-      if (deltaDistance > deltaDistanceThredhold) {
-        ctx.emit("savePosition");
-      }
-    };
-
-    // edge event
-    const onStartEdge = (event: MouseEvent | TouchEvent, direction: NewEdgeEventDirection, index: number) => {
-      isNewEdge.value = true;
-      const { clientX, clientY } = getClientPos(event);
-      ctx.emit("newEdgeStart", {
-        nodeId: props.nodeData.nodeId,
-        x: clientX,
-        y: clientY,
-        index,
-        direction,
-      });
-    };
-    const onEndEdge = () => {
-      isNewEdge.value = false;
-      ctx.emit("newEdgeEnd");
-    };
-    const onMoveEdge = (event: MouseEvent | TouchEvent) => {
-      if (!isNewEdge.value) return;
-      const { clientX, clientY } = getClientPos(event);
-      ctx.emit("newEdge", { x: clientX, y: clientY });
-    };
-    // end of edge event
-
-    watchEffect((onCleanup) => {
-      if (isDragging.value) {
-        window.addEventListener("mousemove", onMoveNode);
-        window.addEventListener("mouseup", onEndNode);
-        window.addEventListener("touchmove", onMoveNode, { passive: false });
-        window.addEventListener("touchend", onEndNode);
-        onCleanup(() => {
-          window.removeEventListener("mousemove", onMoveNode);
-          window.removeEventListener("mouseup", onEndNode);
-          window.removeEventListener("touchmove", onMoveNode);
-          window.removeEventListener("touchend", onEndNode);
-        });
-      }
-    });
-
-    watchEffect((onCleanup) => {
-      if (isNewEdge.value) {
-        window.addEventListener("mousemove", onMoveEdge);
-        window.addEventListener("mouseup", onEndEdge);
-        window.addEventListener("touchmove", onMoveEdge, { passive: false });
-        window.addEventListener("touchend", onEndEdge);
-        onCleanup(() => {
-          window.removeEventListener("mousemove", onMoveEdge);
-          window.removeEventListener("mouseup", onEndEdge);
-          window.removeEventListener("touchmove", onMoveEdge);
-          window.removeEventListener("touchend", onEndEdge);
-        });
-      }
-    });
-
-    const transform = computed(() => {
-      return getTransformStyle(props.nodeData, isDragging.value);
-    });
-    const expectNearNode = computed(() => {
-      return props.nodeData.nodeId === props.nearestData?.nodeId;
-    });
-
-    const isExpectNearButton = (direction: NewEdgeEventDirection, index: number) => {
-      if (!expectNearNode.value) {
-        return false;
-      }
-      return props.nearestData?.direction === direction && props.nearestData?.index === index;
-    };
-
-    let currentWidth = 0;
-    let currentHeight = 0;
-    const focusEvent = () => {
-      if (thisRef.value) {
-        currentWidth = thisRef.value.offsetWidth;
-        currentHeight = thisRef.value.offsetHeight;
-        thisRef.value.style.width = currentWidth * 3 + "px";
-        thisRef.value.style.height = currentHeight * 3 + "px";
-        thisRef.value.style.zIndex = "100";
-      }
-      ctx.emit("updatePosition", getWH());
-    };
-    const blurEvent = () => {
-      if (thisRef.value) {
-        thisRef.value.style.width = currentWidth + "px";
-        thisRef.value.style.height = currentHeight + "px";
-        thisRef.value.style.zIndex = "auto";
-      }
-      ctx.emit("updatePosition", getWH());
-    };
-    const updateStaticValue = (value: UpdateStaticValue) => {
-      ctx.emit("updateStaticNodeValue", value);
-    };
-    const openNodeMenu = (event: MouseEvent) => {
-      ctx.emit("openNodeMenu", event);
-    };
-    const openNodeEditMenu = (event: MouseEvent) => {
-      if (!outerMenu.value) {
-        return;
-      }
-      if (isDragging.value || isNewEdge.value) return;
-      if (deltaDistance > deltaDistanceThredhold) return;
-      ctx.emit("openNodeEditMenu", event);
-    };
+    // Agent selection handlers
     const updateAgentIndex = () => {
       const agent = agentProfile?.agents?.[agentIndex.value];
-      // this is not static node value, but it works
-      ctx.emit("updateStaticNodeValue", { agentIndex: agentIndex.value, agent });
+      emit("updateStaticNodeValue", { agentIndex: agentIndex.value, agent });
     };
+
     watch(
       () => props.nodeData.data.agentIndex,
       (value) => {
@@ -316,14 +132,15 @@ export default defineComponent({
       },
     );
 
+    // Nested graph handlers
     const nestedGraph = computed(() => {
       return store.nestedGraphs[nestedGraphIndex.value];
     });
+
     const updateNestedGraphIndex = () => {
-      ctx.emit("updateNestedGraph", { nestedGraphIndex: nestedGraphIndex.value, nestedGraphId: nestedGraph.value.id });
-      ctx.emit("updatePosition", getWH());
-      // TODO remove Edge
+      emit("updateNestedGraph", { nestedGraphIndex: nestedGraphIndex.value, nestedGraphId: nestedGraph.value.id });
     };
+
     watch(
       () => props.nodeData.data.nestedGraphIndex,
       (value) => {
@@ -333,15 +150,20 @@ export default defineComponent({
       },
     );
 
+    // Static value update handler
+    const updateStaticValue = (value: any) => {
+      emit("updateStaticNodeValue", value);
+    };
+
+    // Inputs/outputs computation
     const inputs = computed(() => {
       if (agentProfile.isNestedGraph) {
-        // not do mapAgent
         return nestedGraphInputs(nestedGraph.value.graph);
       }
       return agentProfile.inputs;
     });
+
     const outputs = computed(() => {
-      // not do mapAgent
       if (agentProfile.isNestedGraph) {
         return nestedGraph.value.graph?.metadata?.forNested?.outputs ?? agentProfile.outputs;
       }
@@ -349,42 +171,26 @@ export default defineComponent({
     });
 
     return {
-      focusEvent,
-      blurEvent,
-
-      transform,
-      onStartNode,
-      isDragging,
+      // State
       innerMenu,
+      outerMenu,
       agentProfile,
-      thisRef,
-      isNewEdge,
-      onStartEdge,
-
-      inputsRef,
-      inputs,
-      outputsRef,
-      outputs,
-
-      expectNearNode,
-      isExpectNearButton,
-
-      updateStaticValue,
-      openNodeMenu,
-      openNodeEditMenu,
-      // helper
-      nodeMainClass,
-      nodeHeaderClass,
-      nodeOutputClass,
-      nodeInputClass,
-
       agentIndex,
-      updateAgentIndex,
-
-      nestedGraphs: store.nestedGraphs,
       nestedGraphIndex,
+
+      // Computed
+      inputs,
+      outputs,
       nestedGraph,
+      nestedGraphs: store.nestedGraphs,
+
+      // Handlers
+      updateAgentIndex,
       updateNestedGraphIndex,
+      updateStaticValue,
+
+      // Helper classes
+      nodeHeaderClass,
     };
   },
 });
---
+++ packages/grapys-vue/src/views/NodeBase.vue
@@ -0,0 +1,296 @@
+<template>
+  <div
+    class="absolute flex w-36 cursor-grab flex-col rounded-md text-center text-white select-none"
+    :class="nodeMainClass(expectNearNode, nodeData)"
+    :style="transform"
+    ref="thisRef"
+    @mousedown="onStartNode"
+    @touchstart="onStartNode"
+    @dblclick="(e) => emit('openNodeMenu', e)"
+    @click="(e) => openNodeEditMenu(e)"
+  >
+    <!-- HEAD -->
+    <div>
+      <slot name="header" :nodeData="nodeData" :expectNearNode="expectNearNode"></slot>
+    </div>
+
+    <!-- Body -->
+    <div>
+      <!-- body head -->
+      <div>
+        <slot name="body-head" :nodeData="nodeData"></slot>
+      </div>
+
+      <!-- right output -->
+      <div class="mt-1 flex flex-col items-end">
+        <div v-for="(output, index) in outputs" :key="['out', output.name, index].join('-')" class="relative flex items-center" ref="outputsRef">
+          <span class="mr-2 text-xs whitespace-nowrap">{{ output.name }}</span>
+          <div
+            class="absolute right-[-10px] h-4 w-4 min-w-[12px] rounded-full"
+            :class="nodeOutputClass(isExpectNearButton('inbound', index), nodeData, isConnectable)"
+            @click.stop
+            @mousedown.stop.prevent="(e) => onStartEdge(e, 'outbound', index)"
+            @touchstart.stop.prevent="(e) => onStartEdge(e, 'outbound', index)"
+          ></div>
+        </div>
+      </div>
+
+      <!-- left input -->
+      <div class="mt-1 mb-1 flex flex-col items-start">
+        <div
+          v-for="(input, index) in inputs"
+          :key="['in', input.name, index, nestedGraphId ?? ''].join('-')"
+          class="relative flex items-center"
+          ref="inputsRef"
+        >
+          <div
+            class="absolute left-[-10px] h-4 w-4 min-w-[12px] rounded-full"
+            :class="nodeInputClass(isExpectNearButton('outbound', index), nodeData, input as any, isConnectable)"
+            @click.stop
+            @mousedown.stop.prevent="(e) => onStartEdge(e, 'inbound', index)"
+            @touchstart.stop.prevent="(e) => onStartEdge(e, 'inbound', index)"
+          ></div>
+          <span class="ml-2 text-xs whitespace-nowrap">{{ input.name }}</span>
+        </div>
+      </div>
+
+      <!-- Body -->
+      <div>
+        <slot name="body-main" :nodeData="nodeData" :focusEvent="focusEvent" :blurEvent="blurEvent"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, watchEffect, computed, PropType, onMounted } from "vue";
+import type { GUINodeData, GUINearestData, NewEdgeEventDirection } from "../utils/gui/type";
+import { getClientPos, getNodeSize, getTransformStyle } from "../utils/gui/utils";
+import { nodeMainClass, nodeHeaderClass, nodeOutputClass, nodeInputClass } from "../utils/gui/classUtils";
+
+export default defineComponent({
+  name: "NodeBase",
+  props: {
+    nodeData: {
+      type: Object as PropType<GUINodeData>,
+      required: true,
+    },
+    nearestData: {
+      type: Object as PropType<GUINearestData>,
+      default: undefined,
+    },
+    inputs: {
+      type: Array as PropType<{ name: string }[]>,
+      required: true,
+    },
+    outputs: {
+      type: Array as PropType<{ name: string }[]>,
+      required: true,
+    },
+    nestedGraphId: {
+      type: String,
+      default: undefined,
+    },
+    isConnectable: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
+  },
+  emits: [
+    "updatePosition",
+    "savePosition",
+    "newEdgeStart",
+    "newEdge",
+    "newEdgeEnd",
+    "openNodeMenu",
+    "openNodeEditMenu",
+    "nodeDragStart",
+    "nodeDragEnd",
+  ],
+  setup(props, { emit }) {
+    const thisRef = ref<HTMLElement | null>(null);
+    const inputsRef = ref<HTMLElement[]>([]);
+    const outputsRef = ref<HTMLElement[]>([]);
+
+    const isDragging = ref(false);
+    const isNewEdge = ref(false);
+    const offset = ref({ x: 0, y: 0 });
+
+    const startPosition = { x: 0, y: 0 };
+    // If it moves only a little, the data will not be saved because it stack much more histories.
+    let deltaDistance = 0; // square
+    const deltaDistanceThredhold = 4;
+
+    const onStartNode = (event: MouseEvent | TouchEvent) => {
+      if (isNewEdge.value) {
+        return;
+      }
+      isDragging.value = true;
+      emit("nodeDragStart");
+      const { clientX, clientY } = getClientPos(event);
+      const position = props.nodeData.position;
+      offset.value.x = clientX - position.x;
+      offset.value.y = clientY - position.y;
+
+      // for update detection
+      startPosition.x = position.x;
+      startPosition.y = position.y;
+      deltaDistance = 0;
+    };
+
+    // some time inputsRef/outputsRef order is broken when nestedGraph change.
+    const sortedInputs = computed(() => {
+      return [...inputsRef.value].sort((aa, bb) => (aa.getBoundingClientRect().top > bb.getBoundingClientRect().top ? 1 : -1));
+    });
+    const sortedOutputs = computed(() => {
+      return [...outputsRef.value].sort((aa, bb) => (aa.getBoundingClientRect().top > bb.getBoundingClientRect().top ? 1 : -1));
+    });
+    const getWH = () => {
+      return getNodeSize(thisRef.value, sortedInputs.value, sortedOutputs.value);
+    };
+    onMounted(() => {
+      emit("updatePosition", getWH());
+    });
+
+    const onMoveNode = (event: MouseEvent | TouchEvent) => {
+      if (!isDragging.value) return;
+      const { clientX, clientY } = getClientPos(event);
+      const x = clientX - offset.value.x;
+      const y = clientY - offset.value.y;
+      const newPosition = { ...getWH(), x, y };
+      emit("updatePosition", newPosition);
+      deltaDistance = (startPosition.x - x) ** 2 + (startPosition.y - y) ** 2;
+    };
+
+    const onEndNode = () => {
+      isDragging.value = false;
+      emit("nodeDragEnd");
+      if (deltaDistance > deltaDistanceThredhold) {
+        emit("savePosition");
+      }
+    };
+
+    // edge event
+    const onStartEdge = (event: MouseEvent | TouchEvent, direction: NewEdgeEventDirection, index: number) => {
+      isNewEdge.value = true;
+      const { clientX, clientY } = getClientPos(event);
+      emit("newEdgeStart", {
+        nodeId: props.nodeData.nodeId,
+        x: clientX,
+        y: clientY,
+        index,
+        direction,
+      });
+    };
+    const onEndEdge = () => {
+      isNewEdge.value = false;
+      emit("newEdgeEnd");
+    };
+    const onMoveEdge = (event: MouseEvent | TouchEvent) => {
+      if (!isNewEdge.value) return;
+      const { clientX, clientY } = getClientPos(event);
+      emit("newEdge", { x: clientX, y: clientY });
+    };
+    // end of edge event
+
+    watchEffect((onCleanup) => {
+      if (isDragging.value) {
+        window.addEventListener("mousemove", onMoveNode);
+        window.addEventListener("mouseup", onEndNode);
+        window.addEventListener("touchmove", onMoveNode, { passive: false });
+        window.addEventListener("touchend", onEndNode);
+        onCleanup(() => {
+          window.removeEventListener("mousemove", onMoveNode);
+          window.removeEventListener("mouseup", onEndNode);
+          window.removeEventListener("touchmove", onMoveNode);
+          window.removeEventListener("touchend", onEndNode);
+        });
+      }
+    });
+
+    watchEffect((onCleanup) => {
+      if (isNewEdge.value) {
+        window.addEventListener("mousemove", onMoveEdge);
+        window.addEventListener("mouseup", onEndEdge);
+        window.addEventListener("touchmove", onMoveEdge, { passive: false });
+        window.addEventListener("touchend", onEndEdge);
+        onCleanup(() => {
+          window.removeEventListener("mousemove", onMoveEdge);
+          window.removeEventListener("mouseup", onEndEdge);
+          window.removeEventListener("touchmove", onMoveEdge);
+          window.removeEventListener("touchend", onEndEdge);
+        });
+      }
+    });
+
+    const transform = computed(() => {
+      return getTransformStyle(props.nodeData, isDragging.value);
+    });
+    const expectNearNode = computed(() => {
+      return props.nodeData.nodeId === props.nearestData?.nodeId;
+    });
+
+    const isExpectNearButton = (direction: NewEdgeEventDirection, index: number) => {
+      if (!expectNearNode.value) {
+        return false;
+      }
+      return props.nearestData?.direction === direction && props.nearestData?.index === index;
+    };
+
+    let currentWidth = 0;
+    let currentHeight = 0;
+    const focusEvent = () => {
+      if (thisRef.value) {
+        currentWidth = thisRef.value.offsetWidth;
+        currentHeight = thisRef.value.offsetHeight;
+        thisRef.value.style.width = currentWidth * 3 + "px";
+        thisRef.value.style.height = currentHeight * 3 + "px";
+        thisRef.value.style.zIndex = "100";
+      }
+      emit("updatePosition", getWH());
+    };
+    const blurEvent = () => {
+      if (thisRef.value) {
+        thisRef.value.style.width = currentWidth + "px";
+        thisRef.value.style.height = currentHeight + "px";
+        thisRef.value.style.zIndex = "auto";
+      }
+      emit("updatePosition", getWH());
+    };
+    const openNodeEditMenu = (event: MouseEvent) => {
+      if (isDragging.value || isNewEdge.value) return;
+      if (deltaDistance > deltaDistanceThredhold) return;
+      emit("openNodeEditMenu", event);
+    };
+
+    return {
+      focusEvent,
+      blurEvent,
+
+      transform,
+      onStartNode,
+      isDragging,
+      thisRef,
+      isNewEdge,
+      onStartEdge,
+
+      inputsRef,
+      outputsRef,
+
+      expectNearNode,
+      isExpectNearButton,
+
+      openNodeEditMenu,
+      // helper
+      nodeMainClass,
+      nodeHeaderClass,
+      nodeOutputClass,
+      nodeInputClass,
+
+      // Expose emit for template
+      emit,
+    };
+  },
+});
+</script>
